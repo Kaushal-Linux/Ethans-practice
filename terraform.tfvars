7cd9c55da372e410adfb66dfdfc1cc71af21bca6
Kaushal Sapelwar <kaushalsapelwar@gmail.com>
2023-09-05 21:47:43 +0530

    Adding variables

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+cidrpub1a = "10.10.1.0/24"
+cidrpriv1a = "10.10.2.0/24"
+region1 = "ap-south-1a"
---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "region1" {
+    type = string
+}
+
+variable "cidrpub1a" {
+    type = string
+}
+
+variable "cidrpriv1a" {
+    type = string
+}
